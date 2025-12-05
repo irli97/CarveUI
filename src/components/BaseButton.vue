5e0a7a1e699ede18db770c64f29c1087b12516ee
Michael <irli.michael@web.de>
2025-12-06 00:08:13 +0100

    style: testing

--- src/components/BaseButton.vue
+++ src/components/BaseButton.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="p-4 rounded text-white text-light-green bg-bright-forest">
+  <div style="margin: 10px" class="p-[89px] rounded text-light-green bg-bright-forest">
     BaseWorld Component
   </div>
 </template>
